ed26dfe939e8cc7a4e905bc30cd43aac9117e8d3
ser dsfdsf <sergey.gutovsk@gmail.com>
2021-04-03 17:13:06 +0300

    service nul

---
+++ service_nul.php
@@ -0,0 +1,200 @@
+<html>
+    <head>
+        <?php require "views/_header.php"; ?>
+    </head>    
+    <body>
+        <?php require "views/_templates.php"; ?>
+
+        <header class="dft-mrg_t_64 dft-container ">
+            <div class="service-header">
+                <div class="service-header__content">
+                    <div class="service-header__info">
+                        <span class="service-header__price dft-text-bold">Цены от 2 500 ₽</span>
+                        <div>
+                            <h1 class="dft-mrg_t_24">Нулевая отчетность</h1>
+                        </div>
+                        <p class="dft-mrg_t_16 dft-block-600">
+                            Даже если компания не ведет финансо-хозяйственную деятельность, 
+                            но по каким то причинам не закрыли компанию, 
+                            Вы обязаны сдавать нулевую отчетность. 
+                            Нулевая отчетность сдается поквартально и ежегодно
+                        </p>
+                        <div class="dft-mrg_t_32">
+                            <a class="dft-btn" href="">Подробнее о тарифах</a>
+                        </div>
+                    </div>
+                    <div class="service-header__image-wrapper">
+                        <img src="imgs/_3.svg" alt="">
+                    </div>
+                </div>
+            </div>
+        </header>
+
+
+        <section class="dft-mrg_t_120 dft-container">
+            <div class="titled-paragraph">
+                <div class="titled-paragraph__info">
+                    <div class="dft-mrg_b_32">
+                        <div class="dft-title">Нулевая отчетность для ИП и ООО на УСН и ОСНО</div>
+                        <div><h3 class="dft-mrg_t_48 dft-text-pd_right">Как сдать нулевую отчетность?</h3></div>
+                    </div>
+                    <div class="dft-mrg_b_32">
+                        <a class="dft-btn" href="">Оставить заявку</a>
+                    </div>
+                </div>
+                <div class="titled-paragraph__body">
+                    <p>
+                        Нулевую отчетность необходимо сдавать по каждому 
+                        отчетному кварталу и по истечении года.
+                        Даже если вы фактически не вели никакой коммерческой 
+                        деятельности, вы обязаны 
+                        <span class="dft-text-bold dft-text-primary">сдать нулевую отчетность!</span>
+                    </p>
+                    <p class="dft-mrg_t_24">
+                        Несвоевременная сдача отчетности грозит вам штрафными санкциями, 
+                        и налоговая инспекция имеет право направить предписание 
+                        в банк о приостановке операций по расчетному счету компании!
+                    </p>
+                    <p class="dft-mrg_t_24">
+                        Несвоевременная сдача отчетности грозит вам штрафными санкциями, 
+                        и налоговая инспекция имеет право направить предписание в банк о приостановке операций по расчетному счету компании!
+                    </p>
+                    <p class="dft-mrg_t_24">
+                        Важно отметить, что доверить подготовку сдачи нулевой отчетности 
+                        необходимо только квалифицированным и проверенным специалистам.
+                        Сегодня многие компании предлагают бухгалтерские услуги недорого, 
+                        но не каждая из них способна помочь успешно сдать квартальную и годовую отчётность.
+                    </p>
+                    <p class="dft-mrg_t_24">
+                        Мы же предоставляем вам все необходимые гарантии. 
+                        Компания «НеГа Консалтинг» поможет сдать нулевой отчёт, а также решит целый ряд смежных задач.
+                    </p>
+                </div>
+            </div>
+        </section>
+
+        <section class="dft-mrg_t_120 dft-container">
+            <div class="service-bonuses dft-wrp-mrg_t_48">
+                <div class="service-bonuses__items-wrapper dft-mrg_t_48">
+                    <div class="service-bonuses__items">
+                        <div class="service-bonuses__col">
+                            <div class="service-bonuses__item dft-mrg_t_48">
+                                <div class="dft-icon-wrapper dft-icon-wrapper_orange">
+                                    <i class="fas fa-bolt"></i>
+                                </div>
+                                <div>
+                                    <div class="dft-text-bold">Это быстро</div>
+                                    <div>
+                                        <div class="dft-text-bold"></div>
+                                        <p class="dft-mrg_t_16">
+                                            В кратчайшие сроки мы подготовим комплект документов 
+                                            для сдачи любого вида нулевой отчетности
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="service-bonuses__item dft-mrg_t_48">
+                                <div class="dft-icon-wrapper dft-icon-wrapper_purple">
+                                    <i class="fas fa-thumbs-up"></i>
+                                </div>
+                                <div>
+                                    <div class="dft-text-bold">Это удобно</div>
+                                    <div>
+                                        <div class="dft-text-bold"></div>
+                                        <p class="dft-mrg_t_16">
+                                            Своевременно подадим документы во все гос. органы, 
+                                            напомним о сроках следующей подачи отчетности
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="service-bonuses__background"></div>
+                        <div class="service-bonuses__col">
+                            <div class="service-bonuses__item">
+                                <div class="dft-icon-wrapper dft-icon-wrapper_green">
+                                    <i class="fas fa-book"></i>
+                                </div>
+                                <div>
+                                    <div class="dft-text-bold">Это профессионально</div>
+                                    <div>
+                                        <div class="dft-text-bold"></div>
+                                        <p class="dft-mrg_t_16">
+                                            Мы отслеживаем все изменения в законодательстве, 
+                                            поэтому все отчеты проходят проверку
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="service-bonuses__info dft-mrg_t_32">
+                    <div class="dft-title">ПРЕИМУЩЕСТВА</div>
+                    <h3 class="dft-mrg_t_48">Выгоды подготовки нулевой отчётности</h3>
+                    <p class="dft-mrg_t_24">
+                        Поможем начинающим предпринимателям сдать квартальный отчёт, и снизим риск возникновения 
+                        различных претензий со стороны налоговых органов
+                    </p>
+                </div>
+            </div>
+        </section>
+
+        <section class="dft-mrg_t_120 dft-container">
+            <div class="dft-block-center dft-target dft-text-center">
+                <div class="dft-title">СПЕЦИАЛЬНОЕ ПРЕДЛОЖЕНИЕ</div>
+                <div class="dft-target__title dft-mrg_t_32">Бесплатная консультация</div>
+                <p class="dft-mrg_t_24">
+                    Подберем лучшие условия обслуживания
+                </p>
+                <a class="dft-mrg_t_32 dft-btn" href="">Позвонить</a>
+            </div>
+        </section>
+
+        <section class="dft-mrg_t_120 dft-container">
+            <div class="feedback">
+                <div class="feedback__info dft-mrg_b_32 dft-block-pd_right">
+                    <h2 class="dft-mrg_b_24">Заказать подготовку нулевой отчетности</h2>
+                    <p class="dft-mrg_b_48">
+                        Для предварительной консультации по возможности 
+                        восстановления бухгалтерского учета 
+                        звоните на горячую линию 
+                        <span class="dft-text-bold dft-text-primary"> +7 (499) 383-09-87</span> 
+                        с понедельника по пятницу с 09:00 до 18:00 или отправьте 
+                        запрос на восстановление бухгалтерского учета через контактную форму
+                    </p>
+                    <a class="dft-btn dft-btn_secondary" href="">Наши контакты</a>
+                </div>
+                <form class="feedback__form">
+                    <div class="dft-form-group">
+                        <label class="dft-form-label" for="">Имя</label>
+                        <input class="dft-form-input" type="text">
+                    </div>
+                    <div class="dft-mrg_t_24 dft-form-group">
+                        <label class="dft-form-label" for="">Телефон</label>
+                        <input class="dft-form-input" type="text">
+                    </div>
+                    <div class="dft-mrg_t_24 dft-form-group">
+                        <label class="dft-form-label" for="">Вопрос или описание проблемы</label>
+                        <textarea class="dft-form-input dft-form-textarea" rows="5" min-rows></textarea>
+                    </div>
+                    <div class="dft-mrg_t_24 dft-form-group dft-form-group_checkbox">
+                        <input class="dft-form-checkbox" type="checkbox">
+                        <label class="dft-form-label">
+                            Я согласен(а) с <a class="dft-link" href="">политикой конфиденциальности</a>
+                        </label>
+                    </div>
+                    <div class="dft-mrg_t_48">
+                        <button class="dft-btn">
+                            Отправить
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </section>
+
+        <?php require "views/templates/_footer.php"; ?>
+    </body>
+
+    <script src="dist/bundle.js"></script>
+</html>
